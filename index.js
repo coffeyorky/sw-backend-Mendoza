182db88b6c2380b4d0de1edbd95755ba5d821283
hunter <rossitamb@hotmail.com>
2023-02-27 22:45:29 -0300

    entregable-mendoza

---
+++ index.js
@@ -0,0 +1,41 @@
+// class ProductManager {
+// }
+
+[
+    {
+    id: 1, 
+    tittle: "Remera Bad batch",
+    description: "Remera de talla universal con estampado de los personajes de la serie The Bad Batch",
+    price: "100",
+    thumbnail: "https://i.ibb.co/VVcTQv2/thbbr.jpg",
+    code: "sw10",
+    stock: "15",
+},
+{
+    id: 2, 
+    tittle: "Taza Star wars",
+    description: "Vasija pequeña de color blanco con el logo de un Trooper en medio",
+    price: "200",
+    thumbnail: "https://i.ibb.co/5k7ZKWY/taza.png",
+    code: "sw11",
+    stock: "15",
+},
+{
+    id: 3, 
+    tittle: "Figura Black Series",
+    description: "Figuras HotToys de rex y cody de Clone Wars - 1/6 scale collectible figure",
+    price: "160",
+    thumbnail: "https://i.ibb.co/NZSXyYt/rex.jpg",
+    code: "sw12",
+    stock: "15",
+},
+{
+    id: 4, 
+    tittle: "Funko Hunter Star Wars",
+    description: "Funko pop de Hunter lider del equipo en la serie The Bad Batch",
+    price: "90",
+    thumbnail: "https://i.ibb.co/pw82S81/funko.jpg",
+    code: "sw13",
+    stock: "15",
+},
+]
