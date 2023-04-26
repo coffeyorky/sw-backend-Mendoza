9e25568a3e1f3f149b4676b6e65674c07ad38af9
hunter <rossitamb@hotmail.com>
2023-04-26 02:50:57 -0300

    SegundaAC-Mendoza

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,8 @@
 const { connect } = require("mongoose");
 const { productsModel } = require("../models/products.model");
 const { cartModel } = require("../models/carts.model");
+const { ordenes } = require("./ordenes");
+const { orderModel } = require("../models/orders.model");
 
 let url =
   "mongodb+srv://Coffeyorky:thebadbatch123@cluster0.j69jxej.mongodb.net/ecommerce?retryWrites=true&w=majority";
@@ -13,30 +15,40 @@ const objConfig = {
     } catch (error) {
       console.log(error)
     }
-    // await productsModel.create({
-    //   title: "Remera Bad batch",
-    //   description:
-    //     "Remera de talla universal con estampado de los personajes de la serie The Bad Batch",
-    //   price: 100,
-    //   thumbnail: "https://i.ibb.co/VVcTQv2/thbbr.jpg",
-    //   stock: 15,
-    //   code: "sw10",
-    //   status: true,
-    // });
-
-    //644488a8c5d37b458e1e7b89
-    // await cartModel.create({
-    //     products: []
-    // })
-
-    // let cart = await cartModel.findById({_id: "6444c09576defc54cd0a9ef2" });
-    // cart.products.push({ product: "644488a8c5d37b458e1e7b89" });
-    // let resp = await cartModel.findByIdAndUpdate({_id: "6444c09576defc54cd0a9ef2"}, cart)
     
-    // console.log(resp)
+    // let result = await orderModel.insertMany(ordenes)
 
-    let cart = await cartModel.find({_id: "6444c09576defc54cd0a9ef2" })
-    console.log(JSON.stringify(cart, null, 2))
+    // let result = await orderModel.find({})
+    // console.log(result)
+
+    const orders = await orderModel.aggregate([
+      {
+        $match: {size: `medium`}
+      },
+      {
+        $group:{_id: `$name`, totalquantity: {$sum: "$quantity"}}
+      },
+      {
+        $sort: {totalquantity: -1 }
+      },
+      // {
+      //   $group: {_id: 1, orders: {$push: `$$ROOT`}}
+      // },
+      {
+        $project:{
+          "_id": 0,
+          orders: "$orders"
+        }
+
+      },
+      {
+         $merge: {
+          into: "reports"
+        }
+      }
+
+    ])
+      console.log(orders)
 
   }
 };
---
+++ src/config/ordenes.js
@@ -0,0 +1,27 @@
+const ordenes = [
+    {
+        name: "Backta",
+        size: "small",
+        price: 8,
+        quantity: 2,
+        date: "2021-01-13T09:08:13Z"
+    },
+    {
+        name: "Peperoni",
+        size: "medium",
+        price: 12,
+        quantity: 1,
+        date: "2021-01-13T09:08:13Z"
+    },
+    {
+        name: "Hawaiian",
+        size: "large",
+        price: 16,
+        quantity: 3,
+        date: "2021-01-13T09:08:13Z"
+    }
+]
+
+module.exports = {
+    ordenes
+}
